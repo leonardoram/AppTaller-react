8822d4768babaa5b4985ee0157c3f2467d92e0b3
leonardo Ramirez <36903903+leonardoram@users.noreply.github.com>
2018-12-13 11:44:09 -0600

    Agregamos cosillas
    Jugando con mas componentes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,69 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Bienvenidos al futuro prros</h1>
-      <h2>nel prro</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { inp: "" };
+    //this.handleInput = this.handleInput.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inp: event.target.value }, () =>
+      console.log("dentro callback", this.state.inp)
+    );
+    console.log(this.state.inp);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>El futuro es ahora viejo ...</h1>
+        <Input change={this.handleChange} />
+        <br />
+        <Etiqueta texto={this.state.inp} />
+        <br />
+        <Escribe />
+        <br />
+        <Descargar />
+      </div>
+    );
+  }
 }
 
+const Input = function(props) {
+  return (
+    <div>
+      <input type="text" onChange={props.change} />
+    </div>
+  );
+};
+
+const Etiqueta = function(props) {
+  return (
+    <div>
+      <p>{props.texto}</p>
+    </div>
+  );
+};
+
+const Escribe = function(props) {
+  return (
+    <div>
+      <input type="button" value="escribe el cuento" onClick={props.change} />
+    </div>
+  );
+};
+
+const Descargar = function(props) {
+  return (
+    <div>
+      <input type="button" value="descargar el cuento " />
+    </div>
+  );
+};
+
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(<App />, rootElement);
-//ReactDom.render(<botonesMenu />,document.getElementById('menu'));
